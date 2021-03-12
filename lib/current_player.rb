0c227e6ea0d5581fb10de300565326c78aa8b7ec
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-12 19:16:58 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,12 @@
 def turn_count(board)
-  
+  counter = 0
+  if 
+    counter += 1
+  end
 end
 # returns number of turns that have been played
 
-def current_player
+def current_player(board)
   
 end
+# use turn_count to determine if "X" or "O" turn
